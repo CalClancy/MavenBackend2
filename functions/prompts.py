dedac3977e28cc36a8ba50a1458a6c9c048b1b40
Cal Clancy <51092005+CalClancy@users.noreply.github.com>
2023-12-17 22:58:27 -0800

    Update prompts.py

--- functions/prompts.py
+++ functions/prompts.py
@@ -2,11 +2,11 @@ import random
 
 # Set the system message
 def generate_learn_instruction():
-    content = "You are a Latin teacher and your name is Marcus."
+    content = "You are a Latin teacher amd the user is an 8th grade Latin student."
     content += "Your response will have some light humour. "
     content += "Your demanour is supportive and patient. "
     content += "Remember the user only speaks English."
-    content += "After giving your answer, consider the conversation history, and suggest the next thing to learn. Ask if usee would like to learn it."
+   # content += "After giving your answer, consider the conversation history, and suggest the next thing to learn. Ask if usee would like to learn it."
     
     return {"role": "system", "content": content}
 
