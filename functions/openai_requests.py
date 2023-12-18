a78bd8313611c8916f8e370336d35ff237725962
Cal Clancy <51092005+CalClancy@users.noreply.github.com>
2023-12-17 16:49:09 -0800

    Update openai_requests.py

--- functions/openai_requests.py
+++ functions/openai_requests.py
@@ -2,48 +2,32 @@ import os
 import openai
 from dotenv import load_dotenv
 from functions.database import get_recent_messages
+from prompts import generate_user_message_prompt
 
 load_dotenv()
 
-
-# Retrieve Enviornment Variables
 openai.organization = os.environ.get("OPEN_AI_ORG")
 openai.api_key = os.environ.get("OPEN_AI_KEY")
 
-
-# Open AI - Whisper
-# Convert audio to text
 def convert_audio_to_text(audio_file):
-  try:
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    message_text = transcript["text"]
-    return message_text
-  except Exception as e:
-    return 
+    try:
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)
+        return transcript["text"]
+    except Exception as e:
+        return 
 
-
-
-# Open AI - Chat GPT
-# Convert audio to text
 def get_chat_response(decoded_message):
     messages = get_recent_messages()
-    user_message = {"role": "user", "content": decoded_message + " Only say two or 3 words in Spanish if speaking in Spanish. The remaining words should be in English"}
+    user_message = generate_user_message_prompt(decoded_message)
     messages.append(user_message)
     print(messages)
 
     try:
         response = openai.ChatCompletion.create(
-          #  model="gpt-4-32k", changed to latest model available in Dec, 2023
-          model="gpt-3.5-turbo-16k",
-          messages=messages
+            model="gpt-3.5-turbo-16k",
+            messages=messages
         )
         message_text = response["choices"][0]["message"]["content"]
-
-        # Check if the type is a string before returning
-        if isinstance(message_text, str):
-            return message_text
-        else:
-            return {"error": "Unexpected response type"}
-
+        return message_text if isinstance(message_text, str) else {"error": "Unexpected response type"}
     except Exception as e:
-        return 
+        return
