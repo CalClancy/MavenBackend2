85d3af776f9928112120c848fe7389af18dfb35b
Cal Clancy <51092005+CalClancy@users.noreply.github.com>
2023-12-17 17:39:26 -0800

    Update prompts.py

--- functions/prompts.py
+++ functions/prompts.py
@@ -1,21 +1,16 @@
 import random
 
+# Set the system message
 def generate_learn_instruction():
-    content = "You are a Spanish teacher and your name is Rachel, the user is called Shaun. Keep responses under 20 words. "
-
-    x = random.uniform(0, 1)
-    if x < 0.2:
-        content += "Your response will have some light humour. "
-    elif x < 0.5:
-        content += "Your response will include an interesting new fact about Spain. "
-    else:
-        content += "Your response will recommend another word to learn. "
-
+    content = "You are a Spanish teacher and your name is Marcus."
+    content += "Your response will have some light humour. "
+    content += "Your demanour is supportive and patient. "
+    
     return {"role": "system", "content": content}
 
 def generate_user_message_prompt(decoded_message):
     user_message = {
         "role": "user", 
-        "content": decoded_message + " Only say two or 3 words in Spanish if speaking in Spanish. The remaining words should be in English"
+        "content": decoded_message + " Only say two or 6 words in Spanish if speaking in Spanish. The remaining words should be in English"
     }
     return user_message
