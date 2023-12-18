1f0fdee05e8adeab1f094aa502189ad5e11eeaf3
Cal Clancy <51092005+CalClancy@users.noreply.github.com>
2023-12-17 22:31:46 -0800

    Update prompts.py

--- functions/prompts.py
+++ functions/prompts.py
@@ -5,6 +5,7 @@ def generate_learn_instruction():
     content = "You are a Latin teacher and your name is Marcus."
     content += "Your response will have some light humour. "
     content += "Your demanour is supportive and patient. "
+    content += "Remember the user only speaks English. So explain the answer also in English so that the user understands the meaning. "
     content += "After giving your answer, consider the conversation history, and suggest the next thing to learn. Ask if use would like to learn it."
     
     return {"role": "system", "content": content}
