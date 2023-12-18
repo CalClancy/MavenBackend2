11283362521808c001ae58e185fc4af81cfcbcdb
Cal Clancy <51092005+CalClancy@users.noreply.github.com>
2023-12-18 00:07:30 +0000

    changed model, voice

--- functions/openai_requests.py
+++ functions/openai_requests.py
@@ -33,7 +33,7 @@ def get_chat_response(decoded_message):
 
     try:
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
+            model="gpt-4-32k", #changed to latest model available in Dec, 2023
             messages=messages
         )
         message_text = response["choices"][0]["message"]["content"]
