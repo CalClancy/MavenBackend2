9f06f47f07cfe7ef5503bf35e124c226a3317de4
Cal Clancy <51092005+CalClancy@users.noreply.github.com>
2023-12-17 22:13:28 -0800

    Update prompts.py

--- functions/prompts.py
+++ functions/prompts.py
@@ -5,7 +5,7 @@ def generate_learn_instruction():
     content = "You are a Latin teacher and your name is Marcus."
     content += "Your response will have some light humour. "
     content += "Your demanour is supportive and patient. "
-    content += "After giving your answer, consider the conversation history, and suggest the next Latin phrase to learn."
+    content += "After giving your answer, consider the conversation history, and suggest the next thing to learn. Ask if use would like to learn it."
     
     return {"role": "system", "content": content}
 
