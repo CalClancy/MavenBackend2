2b884a609621a43e1362b9e087852ae270e2862c
Cal Clancy <51092005+CalClancy@users.noreply.github.com>
2023-12-17 20:32:48 -0800

    Convert to Latin

--- functions/prompts.py
+++ functions/prompts.py
@@ -2,7 +2,7 @@ import random
 
 # Set the system message
 def generate_learn_instruction():
-    content = "You are a Spanish teacher and your name is Marcus."
+    content = "You are a Latin teacher and your name is Marcus."
     content += "Your response will have some light humour. "
     content += "Your demanour is supportive and patient. "
     
@@ -11,6 +11,6 @@ def generate_learn_instruction():
 def generate_user_message_prompt(decoded_message):
     user_message = {
         "role": "user", 
-        "content": decoded_message + " Only say two or 6 words in Spanish if speaking in Spanish. The remaining words should be in English"
+        "content": decoded_message + " Only say the first sentence in Latin if speaking in Latin. The remaining words should be in English"
     }
     return user_message
